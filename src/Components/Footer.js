eac6218645dec30fe893add327c11cc6a9fd47b9
nevrmore <dev.wsameer@gmail.com>
2020-05-16 13:07:17 +0530

    detect user location and get weather data

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <p className="float-right">
           <a href="https://www.linkedin.com/in/wsameer/" rel="noopener noreferrer" target="_blank">Follow me on Linkedin</a>
         </p>
-        <p>This React dashboard project is © <a href="https://github.com/nevrmore" rel="noopener noreferrer" target="_blank">Sameer Waskar</a>, but please fork a branch and customize it for yourself!</p>
+        <p>This React dashboard project is © <a href="https://github.com/wsameer" rel="noopener noreferrer" target="_blank">Sameer Waskar</a>, but please fork a branch and customize it for yourself!</p>
       </div>
     </footer>
   )
--- src/Components/WeatherWidget/WeatherWidget.jsx
+++ src/Components/WeatherWidget/WeatherWidget.jsx
@@ -23,23 +23,46 @@ const WeatherWidget = () => {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
+    fetch('http://ip-api.com/json')
+      .then(res => res.json())
+      .then(response => {
+        if (response.city) {
+          onQueryChange(response.city);
+          getWeatherData(response.city);
+        }
+      })
+      .catch(err => console.log(err));
+
+    return () => {
+      setQuery(null);
+    }
+  }, []);
+
+  useEffect(() => {
     return () => {
       setInterval(() => {
         setCurrentTime(new Date().toLocaleTimeString());
       }, 1000);
     }
-  })
+  });
 
   const onQueryChange = (e) => {
-    setQuery(e.target.value);
+    if (!e) {
+      return false;
+    }
+    if (typeof e === "string") {
+      return setQuery(e);
+    }
+    return setQuery(e.target.value);
   }
 
   /**
    * GET weather data for the user entered city
    */
-  const getWeatherData = () => {
+  const getWeatherData = (city) => {
+    const cityName = city ? city : query;
     let findCity = CityData.filter(city => {
-      return ((city.name).toLowerCase() === query.toLowerCase());
+      return ((city.name).toLowerCase() === cityName.toLowerCase());
     });
 
     if (findCity.length === 0) {
@@ -89,7 +112,6 @@ const WeatherWidget = () => {
   return (
     <div className="weather-container">
       <main>
-
         <h5 className="d-block d-md-none mb-3">Weather App</h5>
 
         <SearchWeather
@@ -99,71 +121,72 @@ const WeatherWidget = () => {
         />
 
         <div className="card weather-card">
-          {(typeof weatherData.main == 'undefined') ? ('') : (
-            <div className="card-body p-0-mob">
-              <div className="media text-center">
-                <div className="media-body">
-                  <h5 className="card-title mt-0 city-name">{weatherData.name.toUpperCase()}</h5>
-                  <h6 className="card-subtitle mb-0 text-muted">{printTodaysDate()}</h6>
-                </div>
-              </div>
-
-              <div className="temp-box text-center">
-                <div className="temp">
-                  <span className="min float-left pl-4">{Math.round(weatherData.main.temp_min)}°</span>
-                  <span className="main">{Math.round(weatherData.main.temp)}</span>
-                  <span className="temp-unit">°C</span>
-                  <span className="min float-right pr-4">{Math.round(weatherData.main.temp_max)}°</span>
+          {(typeof weatherData.main == 'undefined')
+            ? <p className="text-center">No data found</p>
+            : (
+              <div className="card-body p-0-mob">
+                <div className="media text-center">
+                  <div className="media-body">
+                    <h5 className="card-title mt-0 city-name">{weatherData.name.toUpperCase()}</h5>
+                    <h6 className="card-subtitle mb-0 text-muted">{printTodaysDate()}</h6>
+                  </div>
                 </div>
 
-                <div className="weather f-med mb-4">
-                  <img
-                    className="mr-1"
-                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-                    height="42px"
-                    alt="" />
-                  {weatherData.weather[0].main}
-                </div>
-              </div>
+                <div className="temp-box text-center">
+                  <div className="temp">
+                    <span className="min float-left pl-4">{Math.round(weatherData.main.temp_min)}°</span>
+                    <span className="main">{Math.round(weatherData.main.temp)}</span>
+                    <span className="temp-unit">°C</span>
+                    <span className="min float-right pr-4">{Math.round(weatherData.main.temp_max)}°</span>
+                  </div>
 
-              <div className="row mt-4 f-small">
-                <div className="col text-left">
-                  <WeatherDetails
-                    title="Wind Speed"
-                    value={`${weatherData.wind.speed} km/h`}
-                    iconPath={WEATHER_ICONS.wind} />
+                  <div className="weather f-med mb-4">
+                    <img
+                      className="mr-1"
+                      src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                      height="42px"
+                      alt="" />
+                    {weatherData.weather[0].main}
+                  </div>
                 </div>
-                <div className="col text-left">
-                  <WeatherDetails
-                    title="Humidity"
-                    value={`${weatherData.main.humidity} %`}
-                    iconPath={WEATHER_ICONS.humidity} />
-                </div>
-                <div className="w-100"></div>
-                <div className="col text-left">
-                  <WeatherDetails
-                    title="Pressure"
-                    value={weatherData.main.pressure}
-                    iconPath={WEATHER_ICONS.pressure} />
-                </div>
-                <div className="col text-left">
-                  <WeatherDetails
-                    title={convertToLocalTime(weatherData.sys.sunrise)}
-                    value={convertToLocalTime(weatherData.sys.sunset)}
-                    iconPath={WEATHER_ICONS.daynight} />
-                </div>
-              </div>
 
-              <div className="col-sm mt-4 text-center">
-                <div className="card round-card">
-                  <div className="card-body">
-                    <span className="capitaliz">{weatherData.weather[0].description}</span>
+                <div className="row mt-4 f-small">
+                  <div className="col text-left">
+                    <WeatherDetails
+                      title="Wind Speed"
+                      value={`${weatherData.wind.speed} km/h`}
+                      iconPath={WEATHER_ICONS.wind} />
+                  </div>
+                  <div className="col text-left">
+                    <WeatherDetails
+                      title="Humidity"
+                      value={`${weatherData.main.humidity} %`}
+                      iconPath={WEATHER_ICONS.humidity} />
+                  </div>
+                  <div className="w-100"></div>
+                  <div className="col text-left">
+                    <WeatherDetails
+                      title="Pressure"
+                      value={weatherData.main.pressure}
+                      iconPath={WEATHER_ICONS.pressure} />
+                  </div>
+                  <div className="col text-left">
+                    <WeatherDetails
+                      title={convertToLocalTime(weatherData.sys.sunrise)}
+                      value={convertToLocalTime(weatherData.sys.sunset)}
+                      iconPath={WEATHER_ICONS.daynight} />
+                  </div>
+                </div>
+
+                <div className="col-sm mt-4 text-center">
+                  <div className="card round-card">
+                    <div className="card-body">
+                      <span className="capitaliz">{weatherData.weather[0].description}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-
-            </div>
-          )}
+            )}
         </div>
 
       </main>
