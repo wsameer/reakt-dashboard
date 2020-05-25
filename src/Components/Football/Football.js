0d7f7b770203144052593c251e18dc6272f9aad8
nevrmore <dev.wsameer@gmail.com>
2020-05-25 22:37:19 +0530

    file renamed

---
+++ src/Components/Football/Football.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import './Football.css';
+
+const FOOTBALL_API = {
+  key: process.env.REACT_APP_FOOTBALL_API_KEY,
+  url: "https://api.football-data.org/v2/competitions/2021/standings"
+};
+
+// const MCKDATA = dayta;
+
+export class Football extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      standings: [],
+      hasErrors: false,
+      season: null
+    };
+  }
+
+  componentDidMount() {
+    // this.setState({
+    //   loading: false,
+    //   standings: MCKDATA,
+    //   hasErrors: false
+    // });
+
+    this.calculateCurrentSeason();
+
+    fetch(FOOTBALL_API.url, {
+      method: 'GET',
+      headers: { 'X-Auth-Token': FOOTBALL_API.key }
+    })
+      .then(res => res.json())
+      .then(response => {
+        if (response) {
+          let data = response.standings[0].table;
+          this.setState({
+            loading: false,
+            standings: data,
+            hasErrors: false
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ hasErrors: true, loading: false });
+      });
+  }
+
+  clubName = (name) => {
+    return name == null ? '' : name.slice(0, 3).toUpperCase();
+  };
+
+  calculateCurrentSeason = () => {
+    const currentMonth = new Date().getMonth();
+    const currentYear = new Date().getFullYear();
+
+    let currentSeason = currentMonth > 7
+      ? `${currentYear}-${(currentYear + 1).toString().substring(2)}`
+      : `${currentYear - 1}-${(currentYear).toString().substring(2)}`;
+
+    this.setState({
+      season: currentSeason
+    });
+  };
+
+  render() {
+
+    const tableRows = this.state.standings.map((club) =>
+      <tr key={club.team.id}>
+        <th className="no-right-padding" scope="row">{club.position}</th>
+        <td>
+          <span className="badge badge-image-container" data-size="25">
+            <img
+              className="badge-image badge-image--25"
+              src={club.team.crestUrl}
+              alt={club.team.name}
+              height="18"
+            />
+          </span>
+          <span className="long">{this.clubName(club.team.name)}</span>
+        </td>
+        <td>{club.playedGames}</td>
+        <td>{club.won}</td>
+        <td>{club.draw}</td>
+        <td>{club.lost}</td>
+        <td>{club.points}</td>
+      </tr>
+    );
+
+    return (
+      <div className="card p-0">
+        <div className="football-container scrollbar" id="style-1">
+          <div className="card-body">
+            <h5 className="pl-2 card-title">Premier League ({this.state.season})</h5>
+            {(this.loading === true) ? ('') : (
+              <table className="table table-borderless table-hover white-table">
+                <thead>
+                  <tr>
+                    <th scope="col">Pos</th>
+                    <th scope="col" className="pleft-4">Club</th>
+                    <th scope="col">P</th>
+                    <th scope="col">W</th>
+                    <th scope="col">D</th>
+                    <th scope="col">L</th>
+                    <th scope="col">Pts</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableRows}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Football;
