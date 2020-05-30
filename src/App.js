b54d45ddfa36e23b1f01f989bc762230d0983961
nevrmore <dev.wsameer@gmail.com>
2020-05-30 19:49:04 +0530

    added suspense and fallback

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import './App.scss'
-import WeatherWidget from './Components/WeatherWidget/WeatherWidget'
-import TodoList from './Components/TodoList/TodoList'
-import Football from './Components/Football/Football'
-import Footer from './Components/Footer'
+import React, { Suspense } from 'react';
+import './App.scss';
+import BusyIndicator from './Components/BusyIndicator';
+const WeatherWidget = React.lazy(() => import('./Components/WeatherWidget/WeatherWidget'));
+const TodoList = React.lazy(() => import('./Components/TodoList/TodoList'));
+const Football = React.lazy(() => import('./Components/Football/Football'));
+const Footer = React.lazy(() => import('./Components/Footer'));
 
 function App() {
   return (
-    <div className="app container-fluid">
-      <div className="round-square">
-        <div className="row">
-          <div className="col-sm-4 p-0 left-side">
-            <WeatherWidget />
-          </div>
-          <div className="row col-sm-8 right-side components">
-            <div className="col-sm todo-main">
-              <TodoList />
+    <Suspense fallback={<BusyIndicator />}>
+      <div className="app container-fluid">
+        <div className="round-square">
+          <div className="row">
+            <div className="col-sm-4 p-0 left-side">
+              <WeatherWidget />
             </div>
-            <div className="col-sm football-main">
-              <Football />
+            <div className="row col-sm-8 right-side components">
+              <div className="col-sm todo-main">
+                <TodoList />
+              </div>
+              <div className="col-sm football-main">
+                <Football />
+              </div>
             </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
-    </div>
+    </Suspense>
   );
-}
+};
 
 export default App;
