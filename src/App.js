3dc9492acd9b9816adc99299508bcb1480644e3a
nevrmore <dev.wsameer@gmail.com>
2020-05-31 16:55:07 +0530

    added layout component for cleaner code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Suspense } from 'react';
 import './App.scss';
 import BusyIndicator from './Components/BusyIndicator';
-const WeatherWidget = React.lazy(() => import('./Components/WeatherWidget/WeatherWidget'));
-const TodoList = React.lazy(() => import('./Components/TodoList/TodoList'));
-const Football = React.lazy(() => import('./Components/Football/Football'));
-const Footer = React.lazy(() => import('./Components/Footer'));
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Layout from './Components/Layout';
 
 function App() {
   return (
     <Suspense fallback={<BusyIndicator />}>
       <div className="app container-fluid">
+        <Header />
         <div className="round-square">
-          <div className="row">
-            <div className="col-sm-4 p-0 left-side">
-              <WeatherWidget />
-            </div>
-            <div className="row col-sm-8 right-side components">
-              <div className="col-sm todo-main">
-                <TodoList />
-              </div>
-              <div className="col-sm football-main">
-                <Football />
-              </div>
-            </div>
-          </div>
+          <Layout />
           <Footer />
         </div>
       </div>
---
+++ src/Components/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WeatherWidget = React.lazy(() => import('./WeatherWidget/WeatherWidget'));
+const TodoList = React.lazy(() => import('./TodoList/TodoList'));
+const Football = React.lazy(() => import('./Football/Football'));
+
+const Layout = (props) => {
+  return (
+    <div className="row">
+      <div className="col-sm-4 p-0 left-side">
+        {/* <WeatherWidget /> */}
+      </div>
+      <div className="row col-sm-8 right-side components">
+        <div className="col-sm todo-main">
+          <TodoList />
+        </div>
+        <div className="col-sm football-main">
+          {/* <Football /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Layout.propTypes = {
+
+};
+
+export default Layout;
