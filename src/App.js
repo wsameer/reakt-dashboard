3d27db0de196a5de2a2afbcd9d6072d2a5e30f9a
wsameer <dev.wsameer@gmail.com>
2020-09-23 20:19:14 +0530

    Removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import './App.scss';
 import BusyIndicator from './Components/BusyIndicator';
-import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Layout from './Components/Layout';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <Suspense fallback={<BusyIndicator />}>
       <div className="app container-fluid p-0">
-        {/* <Header darkMode={darkMode} /> */}
         <div className="round-square">
           <Layout />
           <Footer />
