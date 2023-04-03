40be0c1cf6930e494ac68e09d3760c0f4b6d1b4c
wsameer <dev.wsameer@gmail.com>
2023-04-03 19:07:36 -0400

    feat: removes css and adds scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from 'react';
+import Footer from './components/Footer';
+import BusyIndicator from './components/BusyIndicator';
+import PageLayout from './components/layout/PageLayout';
+import Weather from './components/Weather/Weather';
+import Todo from './components/Todo/Todo';
+import Football from './components/Football/Football';
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	return (
+		<Suspense fallback={<BusyIndicator />}>
+			<PageLayout>
+				<Weather />
+				<div className="row col-8 right-side components">
+					<Todo />
+					<Football />
+				</div>
+			</PageLayout>
+			<Footer />
+		</Suspense>
+	);
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+export default App;
