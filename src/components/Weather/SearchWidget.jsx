5f12bb9e1d1fba336bbae208521dfbf4e6211096
wsameer <dev.wsameer@gmail.com>
2023-04-04 11:24:40 -0400

    feat: adds component to search weather per location

---
+++ src/components/Weather/SearchWidget.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchWeather = ({ query, getWeatherData, onQueryChange, disabled }) => {
+	const keyPressEvent = event => {
+		event.preventDefault();
+		if (event.keyCode === 13 && query.length > 3) {
+			getWeatherData();
+		}
+	};
+
+	return (
+		<div className="form-group search-box">
+			<input
+				disabled={disabled}
+				type="text"
+				className="form-control form-control-lg"
+				placeholder="Search for a city..."
+				aria-describedby="emailHelp"
+				onChange={onQueryChange}
+				value={query}
+				onKeyUp={keyPressEvent}
+			/>
+		</div>
+	);
+};
+
+SearchWeather.propTypes = {
+	query: PropTypes.string,
+	getWeatherData: PropTypes.func.isRequired,
+	onQueryChange: PropTypes.func.isRequired,
+	disabled: PropTypes.bool
+};
+
+export default SearchWeather;
