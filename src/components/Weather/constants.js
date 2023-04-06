40764e84e702dc13e7555685092d6a44207c218d
wsameer <dev.wsameer@gmail.com>
2023-04-05 21:00:58 -0400

    feat: adds constants file

---
+++ src/components/Weather/constants.js
@@ -0,0 +1,11 @@
+export const WEATHER_API = {
+	key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+	base: 'https://api.openweathermap.org/data/2.5/'
+};
+
+export const WEATHER_ICONS = {
+	wind: require('../../assets/images/icons8-wind-50.png'),
+	humidity: require('../../assets/images/icons8-wet-50.png'),
+	daynight: require('../../assets/images/icons8-day-and-night-50.png'),
+	pressure: require('../../assets/images/icons8-atmospheric-pressure-50.png')
+};
