74bb15c4594184bbaac35a9841a22a15363c9dfb
wsameer <dev.wsameer@gmail.com>
2023-04-07 10:08:21 -0400

    fix: removes useWeather references

--- src/components/Weather/SearchWeather.jsx
+++ src/components/Weather/SearchWeather.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useWeather } from './useWeather';
 
-const SearchWeather = ({ query, getWeatherData, onQueryChange, disabled }) => {
+const SearchWeather = ({ getWeatherData, isLoading, onQueryChange }) => {
+
+	const [query, setQuery] = useState('');
+
+	/**
+	 * Handles on query change event
+	 * @param {object} event The DOM object
+	 */
+	const onQueryChange = event => {
+		if (!event) {
+			return false;
+		}
+		if (typeof event === 'string') {
+			return setQuery(event);
+		}
+		return setQuery(event.target.value);
+	};
+
 	const keyPressEvent = event => {
 		event.preventDefault();
 		if (event.keyCode === 13 && query.length > 3) {
@@ -12,7 +30,7 @@ const SearchWeather = ({ query, getWeatherData, onQueryChange, disabled }) => {
 	return (
 		<div className="form-group search-box">
 			<input
-				disabled={disabled}
+				disabled={isLoading}
 				type="text"
 				className="form-control form-control-lg"
 				placeholder="Search for a city..."
@@ -26,10 +44,9 @@ const SearchWeather = ({ query, getWeatherData, onQueryChange, disabled }) => {
 };
 
 SearchWeather.propTypes = {
-	query: PropTypes.string,
 	getWeatherData: PropTypes.func.isRequired,
 	onQueryChange: PropTypes.func.isRequired,
-	disabled: PropTypes.bool
+	isLoading: PropTypes.bool.isRequired
 };
 
 export default SearchWeather;
