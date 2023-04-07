08d9803a47a0338e0f9809c912fa3c93fe36cbef
wsameer <dev.wsameer@gmail.com>
2023-04-07 10:09:08 -0400

    refactor: removes useReducer related constants

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SearchWeather from './SearchWeather';
+import './Weather.scss';
 
 const Weather = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [weatherData, setWeatherData] = useState([]);
+
+	/**
+	 * GET weather data for the user entered city
+	 * @param {string} city The name of the city
+	 */
+	const getWeatherData = city => {
+		setIsLoading(true);
+		const cityName = city ? city : query;
+		let findCity = CityData.find(
+			city => city.name.toLowerCase() === cityName.toLowerCase()
+		);
+
+		if (!findCity || findCity.length === 0) {
+			setQuery('');
+			setWeatherData({});
+			return handleApiError('No record found');
+		}
+
+		fetch(
+			`${WEATHER_API.base}weather?id=${findCity.id}&units=metric&APPID=${WEATHER_API.key}`
+		)
+			.then(res => res.json())
+			.then(result => {
+				setIsLoading(false);
+				if (result.cod !== 200) {
+					return handleApiError(result.message);
+				}
+				setQuery('');
+				setWeatherData(result);
+			});
+	};
+
 	return (
-		<div className="col-4 p-0 left-side">
-			<h1 className="text-white">Weather Widget</h1>
+		<div className="weather-container">
+			<main>
+				<h5 className="d-block d-md-none mb-3">Weather App</h5>
+				<SearchWeather
+					query={''}
+					onQueryChange={() => null}
+					getWeatherData={getWeatherData}
+					isLoading={isLoading}
+				/>
+			</main>
 		</div>
 	);
 };
--- src/components/Weather/constants.js
+++ src/components/Weather/constants.js
@@ -1,7 +1,8 @@
-// export const WEATHER_API = {
-// 	key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
-// 	base: 'https://api.openweathermap.org/data/2.5/'
-// };
+export const WEATHER_API = {
+	// key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+	key: '',
+	base: 'https://api.openweathermap.org/data/2.5/'
+};
 
 // export const WEATHER_ICONS = {
 // 	wind: require('../../assets/images/icons8-wind-50.png'),
@@ -9,9 +10,3 @@
 // 	daynight: require('../../assets/images/icons8-day-and-night-50.png'),
 // 	pressure: require('../../assets/images/icons8-atmospheric-pressure-50.png')
 // };
-
-export const weatherActions = {
-	search: 'SEARCH',
-	clear: 'CLEAR',
-	isLoading: 'ISLOADING'
-};
