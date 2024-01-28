ef38646e59dbcbeaca594cf6fd9550822079d83d
wsameer <dev.wsameer@gmail.com>
2024-01-28 16:44:43 -0500

    updated fetcher to accept mock and query parameters

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,15 @@
 import mockData from '../components/Weather/mockData.json';
 
-export const fetcher = url => fetch(url).then(r => r.json());
-
-export const mockGetWeather = async () => {
+const mockGetWeather = async () => {
 	return new Promise(resolve =>
 		setTimeout(() => {
 			resolve(mockData);
 		}, 2000)
 	);
+};
+
+export const fetcher = (url, queryParams = '', wantMock = false) => {
+	return wantMock
+		? mockGetWeather()
+		: fetch(`${url}${queryParams}`).then(r => r.json());
 };
